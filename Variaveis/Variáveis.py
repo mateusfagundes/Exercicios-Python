2b877d2f2c56a063277a58e096a5db92fa46732c
Mateus Fagundes <57625884+mateusfagundes@users.noreply.github.com>
2021-05-12 15:17:34 -0300

    Add files via upload

---
+++ Variaveis/Variáveis.py
@@ -0,0 +1,13 @@
+a = "Olá mundo!"
+print(a)
+
+var1 = 1 # variável inteira
+var2 = 1.1 # variável float
+var3 = "I'm String" #variável string
+var4 = True #verdadeiro
+var5 = False #falso
+
+print(var1)
+print(var2)
+print(var3)
+print(var4)
