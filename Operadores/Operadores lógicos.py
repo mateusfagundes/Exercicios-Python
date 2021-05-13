0b0cccf0eea29803bd3c791084f5d12621ff29e1
Mateus Fagundes <57625884+mateusfagundes@users.noreply.github.com>
2021-05-13 11:27:05 -0300

    Add files via upload

---
+++ Operadores/Operadores lógicos.py
@@ -0,0 +1,11 @@
+#Operadores relacionais
+
+x = 2
+y = 3
+soma = x + y
+print(x == y)
+# Falso x não é == a y
+print(x > y)
+# Falso x não é > que y
+print(soma >= y)
+# Verdadeiro soma é > ou = a y
---
+++ Operadores/Operadores relacionais.py
@@ -0,0 +1,9 @@
+#Operadores lógicos AND e OR
+
+x = 3
+y = 3
+z = 4
+
+print(x == y and x == z)
+print(x == y or x == z)
+print(x == y or x == z and z == y)
