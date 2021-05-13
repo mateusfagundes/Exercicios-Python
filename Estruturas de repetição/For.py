8871abe68ac43443e0d67466bb84918b4a9ae79a
Mateus Fagundes <57625884+mateusfagundes@users.noreply.github.com>
2021-05-13 11:29:45 -0300

    Add files via upload

---
+++ Estruturas de repetição/For.py
@@ -0,0 +1,12 @@
+lista1 = [1,2,3,4,5]
+lista2 = ["olá","mundo","!"]
+lista3 = [0,"olá",9.99,True]
+
+for i in lista1:
+    print(i)
+
+for i in lista2:
+    print(i)
+
+for i in lista3:
+    print(i)
