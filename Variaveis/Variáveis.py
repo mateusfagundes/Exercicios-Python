93306dfa10f387afa64483f078432d43a2d538cf
Mateus Fagundes <57625884+mateusfagundes@users.noreply.github.com>
2021-05-13 12:24:43 -0300

    Add files via upload
    atualizado 13/05/21

--- Variaveis/Variáveis.py
+++ Variaveis/Variáveis.py
@@ -3,11 +3,25 @@ print(a)
 
 var1 = 1 # variável inteira
 var2 = 1.1 # variável float
-var3 = "I'm String" #variável string
-var4 = True #verdadeiro
-var5 = False #falso
+var3 = " I'm String" #variável string
+var4 = True # verdadeiro
+var5 = False # falso
+PI = 3.14
 
 print(var1)
 print(var2)
 print(var3)
 print(var4)
+
+x = """Transformando Comentário em Variável"""
+
+print(x)
+
+#'Somando' as variaveis
+
+print(x + str(var3)) 
+print (f'{var3} {var1 + var2}') # Referenciou os valores e somou
+
+raio = float(input('informe a circunferência:'))
+area = PI * raio * raio
+print(f'A área da circunferência é {area} m².')
